72afaf0a379349cfce35e8eecf8561ffed36f4b2
Isidro Cuate <jrvelez724@gmail.com>
2021-02-13 02:34:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
-def create_an_empty_array[]
-  
+def create_an_empty_array
+  []
 end
 
 def create_an_array
-  
+  ["Junior", "Sebastian", "Iselly"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push "Dog"
 end
 
 def add_element_to_start_of_array(array, element)
